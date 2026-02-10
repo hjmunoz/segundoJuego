671ec2b0be5fc53f290e1d443d80f6dafebf1942
hjmunoz <hector.jefrey@gmail.com>
2026-02-10 18:28:09 -0500

    se agregan nuevas funcionalidades, una arma, se cambia pj, y se cambia estructura del juego.

---
+++ camera_2d.gd
@@ -0,0 +1,13 @@
+extends Camera2D
+
+@export var objetivo:Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if objetivo:
+		position = objetivo.position
--- player.gd
+++ player.gd
@@ -1,31 +1,37 @@
-extends Area2D
+extends CharacterBody2D
 
-@export var speed = 400
-var screen_size
+const SPEED = 150.0
+const JUMP_VELOCITY = -400.0
 
+func _physics_process(delta: float) -> void:
+	# 1. Gravedad
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	screen_size = get_viewport_rect().size
+	# 2. Salto (Flecha arriba)
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
+	# 3. Ataque (Espacio)
+	if Input.is_action_just_pressed("ui_accept"):
+		$ataque.play("atacar")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("move_right")
-		velocity.x += 1
-	if Input.is_action_pressed("move_left")
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down")
-		velocity.x += 1
-	if Input.is_action_pressed("move_up")
-		velocity.x -= 1
+	# 4. Movimiento (D, A y Flechas)
+	# Usamos "ui_left" y "ui_right" porque Godot ya incluye A, D y Flechas ahí
+	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	if direction != 0:
+		velocity.x = direction * SPEED
+		$AnimatedSprite2D.flip_h = (direction < 0)
+		$AnimatedSprite2D.play("player2")
 		
-	if velocity.length() > 0
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
+		# --- AJUSTE DE LA ESPADA ---
+		# Si se aleja mucho, cambia el 7 por un número más pequeño como 5
+		$Espada.position.x = 4 * direction  
+		$Espada.scale.x = 0.04 * direction
+		$Espada.z_index = 1 
 	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 		$AnimatedSprite2D.stop()
-		
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+
+	move_and_slide()
